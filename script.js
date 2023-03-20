52c0d364df0273eff82024233a31055aeaa7bca5
Elisma2711 <ismael_aloy@hotmail.com>
2023-03-20 12:33:31 -0300

    v0.1.2 Embellecimiento del js

--- script.js
+++ script.js
@@ -2,7 +2,13 @@ let details = navigator.userAgent;
 let regexp = /android|iphone|kindle|ipad/i;
 let isMobileDevice = regexp.test(details);
 if (isMobileDevice) {
-    document.write('<header id="headerMobile"><img class="logoHeader" src="resources/logomezupng.png" alt="Logo de MEZU Whisky"></header>');
+    document.write('\
+    <header id="headerMobile">\
+     <img class="logoHeader" src="resources/logomezupng.png" alt="Logo de MEZU Whisky">\
+    </header>');
 } else {
-    document.write('<header id="headerDesktop"><img class="logoHeader" src="resources/logomezupng.png" alt="Logo de MEZU Whisky"></header>');
+    document.write('\
+    <header id="headerDesktop">\
+     <img class="logoHeader" src="resources/logomezupng.png" alt="Logo de MEZU Whisky">\
+    </header>');
 }
